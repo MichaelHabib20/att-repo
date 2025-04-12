59de3a28a4c7e1a9f3900de40bebbc0455026019
MichaelHabib20 <Michael.Ramzy@supercommerce.io>
2025-04-12 20:14:55 +0200

    Update Vite configuration to change production base path from '/malyka-frontend/' to '/att-repo/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/malyka-frontend/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/att-repo/' : '/',
   plugins: [
     vue(),
     VitePWA({
